a072e02929666e97bd93cf3c8879690047f61dc7
PraveshC92 <64627924+PraveshC92@users.noreply.github.com>
2025-01-09 19:45:07 +0530

    modifications

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 5000 });
+// const WebSocket = require("ws");
+// const wss = new WebSocket.Server({ port: 5000 });
 
-wss.on("connection", (ws) => {
-  ws.on("message", (msg) => {
-    console.log(`Received:%s`, msg);
-  });
+// wss.on("connection", (ws) => {
+//   ws.on("message", (msg) => {
+//     console.log(`Received:%s`, msg);
+//   });
 
-  ws.send("something");
+//   ws.send("something");
+// });
+const http = require("http");
+const fs = require("fs");
+
+fs.readFile("./home.html", (err, data) => {
+  if (err) throw err;
+
+  http
+    .createServer((req, res) => {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(data);
+      res.end();
+    })
+    .listen(8000, console.log(`app is listening on the port 8000`));
 });
